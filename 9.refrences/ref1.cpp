0906a1f0818be973404f5b4defe0c370107366ee
bunty chauhan <buntychauhan1464@gmail.com>
2022-11-08 13:45:27 +0530

    bits file added

---
+++ 9.refrences/ref1.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+    // freopen("input.txt", "r",stdin);
+    // freopen("output.txt", "w",stdout);
+
+    int v1=34;
+    int &ref=v1;
+    cout<<v1<<endl;
+    cout<<ref<<endl;
+    
+
+
+    return 0;
+}
